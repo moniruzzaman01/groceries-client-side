9a853103680851a649d357390d2534178166161a
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-04-29 10:14:52 +0600

    footer fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,30 +6,16 @@ const Footer = () => {
   const year = date.getFullYear();
   return (
     <footer className="footer-container">
-      <div className="copyright-container">
-        <div style={{ width: "130px" }} className="middle mx-auto">
+      <div className="brand toggle">
+        <div>
           <h2>Groceries</h2>
           <small>Your daily needs</small>
         </div>
-        <p className="copyright">
-          Copyright-<span className="name">Moniruzzaman</span>{" "}
-          <span style={{ color: "tomato" }}>@{year}</span>
-        </p>
       </div>
-      <div className="location mx-auto">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores cum
-        maxime tenetur suscipit vitae a! Exercitationem unde asperiores vitae,
-        ad provident ea perferendis explicabo velit laboriosam nam quis cum?
-        Totam.
-      </div>
-      <div className="social-icons mx-auto">
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-          quasi voluptatum quaerat atque ipsam nesciunt pariatur consectetur
-          quia amet animi, laborum quo modi. Nesciunt in, officia optio ipsam
-          culpa laborum!
-        </p>
-      </div>
+      <p className="copyright">
+        Copyright-<span className="name">Moniruzzaman</span>{" "}
+        <span style={{ color: "tomato" }}>@{year}</span>
+      </p>
     </footer>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
         <div className="brand-container">
           <div className="brand toggle">
             <h2>Groceries</h2>
@@ -13,7 +13,7 @@ const Header = () => {
           </div>
         </div>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -21,29 +21,29 @@ const Header = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
               <Link to="/home">Home</Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link to="/my-item">My Items</Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link to="/login">Login</Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link to="/add-item">Add Item</Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link to="/manage-item">Manage Item</Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link to="/logout">LogOut</Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link to="/sign-up">SignUp</Link>
             </li>
           </ul>
