4b8ebfe3a5d9fda115d0f0669259c884cb44dfd1
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-04-30 23:49:42 +0600

    onlick item deletion implimented

--- src/components/inventory-items/InventoryItems.js
+++ src/components/inventory-items/InventoryItems.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { toast } from "react-toastify";
 import useInventoryItems from "../../hooks/useInventoryItems";
 import "./InventoryItems.css";
 
 const InventoryItems = () => {
-  const [items] = useInventoryItems();
+  const [items, setItems] = useInventoryItems();
 
+  const handleDelete = (id) => {
+    // console.log(id);
+    fetch(`http://localhost:5000/deleteById/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount === 1) {
+          toast("item deleted successfully!");
+          const rest = items.filter((item) => item._id !== id);
+          setItems(rest);
+        }
+      });
+  };
   return (
     <div className="inventory-item-container">
       <h1
@@ -33,7 +48,12 @@ const InventoryItems = () => {
               <td>{item.price}/-</td>
               <td>{item.quantity}</td>
               <td>{item.description}</td>
-              <td style={{ color: "tomato", cursor: "pointer" }}>Delete</td>
+              <td
+                onClick={() => handleDelete(item._id)}
+                style={{ color: "tomato", cursor: "pointer" }}
+              >
+                Delete
+              </td>
             </tr>
           ))}
         </tbody>
