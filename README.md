acddefa8652c53297efb74c4a45a31ea92f2e264
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-05-01 01:31:16 +0600

    readme file and blog component edited

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# Getting Started with Create React App
+# SiteName: Groceries
+
+## SiteLink: [Visit Site](https://groceries-59179.web.app/)
+
+#### This site is based on groceries warehouse. Where you will find all your daily grocery and your others daily needs.
+
+### Key points about This site
+
+- This site is created with React
+- Mongodb is used to maintain database works
+- For authentication system it has firebase auth system
+- This site will manage all of your calculation about all of your products, customers and suppliers.
+- Last it is easy to use.
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,37 @@
 import React from "react";
+import "./Blog.css";
 
 const Blogs = () => {
   return (
-    <div>
-      <h1>Blogs</h1>
+    <div className="blog-item-container">
+      <h1 className="mb-4">Blogs</h1>
+      <div>
+        <h4>Question:</h4>
+        <p>
+          Answer: Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Corporis accusamus error harum dicta, voluptatem neque autem unde
+          necessitatibus quas sed quia optio numquam fugit, delectus aperiam
+          incidunt consequatur iste voluptate!
+        </p>
+      </div>
+      <div>
+        <h4>Question:</h4>
+        <p>
+          Answer: Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Corporis accusamus error harum dicta, voluptatem neque autem unde
+          necessitatibus quas sed quia optio numquam fugit, delectus aperiam
+          incidunt consequatur iste voluptate!
+        </p>
+      </div>
+      <div>
+        <h4>Question:</h4>
+        <p>
+          Answer: Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Corporis accusamus error harum dicta, voluptatem neque autem unde
+          necessitatibus quas sed quia optio numquam fugit, delectus aperiam
+          incidunt consequatur iste voluptate!
+        </p>
+      </div>
     </div>
   );
 };
