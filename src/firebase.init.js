4383e47618db6ae6bde836c61ad575065ad5832e
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-04-30 21:19:18 +0600

    firebase file edited, environment variable added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,13 +5,21 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC0Y7nVbnnhLz2MmTt2oWTnDeT8bKN8Es8",
+//   authDomain: "groceries-59179.firebaseapp.com",
+//   projectId: "groceries-59179",
+//   storageBucket: "groceries-59179.appspot.com",
+//   messagingSenderId: "558200529369",
+//   appId: "1:558200529369:web:0adc996e11854671ff79c8",
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyC0Y7nVbnnhLz2MmTt2oWTnDeT8bKN8Es8",
-  authDomain: "groceries-59179.firebaseapp.com",
-  projectId: "groceries-59179",
-  storageBucket: "groceries-59179.appspot.com",
-  messagingSenderId: "558200529369",
-  appId: "1:558200529369:web:0adc996e11854671ff79c8",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
