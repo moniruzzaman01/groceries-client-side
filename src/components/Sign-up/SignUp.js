2ad05da733bdc3c2c7080c687db03455f2df2038
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-05-04 19:30:59 +0600

    update user implimented using firebase hooks

--- src/components/Sign-up/SignUp.js
+++ src/components/Sign-up/SignUp.js
@@ -5,92 +5,97 @@ import SocialLogin from "../Social-login/SocialLogin";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
 import Spineer from "../Spineer/Spineer";
+import { useUpdateProfile } from "react-firebase-hooks/auth";
 
 const SignUp = () => {
   const [createUserWithEmailAndPassword, user, loading, error] =
     useCreateUserWithEmailAndPassword(auth, {
       sendEmailVerification: true,
     });
+  const [updateProfile, updating] = useUpdateProfile(auth);
   const location = useLocation();
   const navigate = useNavigate();
   const from = location.state?.from?.pathname || "/";
 
-  const handleFormSubmit = (event) => {
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
+    const name = event.target.name.value;
     const email = event.target.email.value;
     const password = event.target.pass.value;
 
-    createUserWithEmailAndPassword(email, password);
+    await createUserWithEmailAndPassword(email, password);
+    updateProfile({ displayName: name });
   };
   if (user) {
     navigate(from, { replace: true });
   }
   return (
-    <div className="signup-form-container">
-      <h1>Please Register Here!!!</h1>
-      <form onSubmit={handleFormSubmit}>
-        <div className="form-group mb-3">
-          <label htmlFor="exampleInputName1">Your Name</label>
-          <input
-            type="text"
-            name="name"
-            className="form-control"
-            id="exampleInputName1"
-            placeholder="Your Name"
-          />
-        </div>
-        <div className="form-group mb-3">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input
-            type="email"
-            name="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            placeholder="Enter email"
-            required
-          />
-        </div>
-        <div className="form-group mb-3">
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            name="pass"
-            className="form-control"
-            id="exampleInputPassword1"
-            placeholder="Password"
-            required
-          />
-        </div>
-        <p>
-          Have an account?{" "}
-          <Link className="link" to="/login">
-            Login
-          </Link>
-        </p>
-
-        {loading ? <Spineer /> : ""}
-        {error ? (
-          <p
-            style={{
-              color: "blue",
-            }}
-          >
-            {error.message}
+    <>
+      {loading || updating ? <Spineer /> : ""}
+      <div className="signup-form-container">
+        <h1>Please Register Here!!!</h1>
+        <form onSubmit={handleFormSubmit}>
+          <div className="form-group mb-3">
+            <label htmlFor="exampleInputName1">Your Name</label>
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              id="exampleInputName1"
+              placeholder="Your Name"
+            />
+          </div>
+          <div className="form-group mb-3">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter email"
+              required
+            />
+          </div>
+          <div className="form-group mb-3">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              name="pass"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <p>
+            Have an account?{" "}
+            <Link className="link" to="/login">
+              Login
+            </Link>
           </p>
-        ) : (
-          ""
-        )}
-        <button type="submit" className="btn form-btn">
-          SignUp
-        </button>
-      </form>
-      <div className="d-flex justify-content-center align-items-center my-4">
-        <div className="separator"></div>
-        <div className="mx-2">or</div>
-        <div className="separator"></div>
+          {error ? (
+            <p
+              style={{
+                color: "blue",
+              }}
+            >
+              {error.message}
+            </p>
+          ) : (
+            ""
+          )}
+          <button type="submit" className="btn form-btn">
+            SignUp
+          </button>
+        </form>
+        <div className="d-flex justify-content-center align-items-center my-4">
+          <div className="separator"></div>
+          <div className="mx-2">or</div>
+          <div className="separator"></div>
+        </div>
+        <SocialLogin />
       </div>
-      <SocialLogin />
-    </div>
+    </>
   );
 };
 
